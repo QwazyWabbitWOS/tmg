c1bc868ce2576e73545f7d645f9f03b0f86f538a
Geoff <gljoy@mac.com>
2016-05-14 15:19:28 -0700

    Massive update from insane QwazyWabbit. Many changes for style, format, portability but this produces 'clean' compile for Windows, Linux, OS X. Some bugs squashed.

--- TMG/vote.c
+++
@@ -1,9 +0,0 @@
-/*vote.c
-Contains all the vote handeling functions
-4/21/01 RaVeN
-*/
-#include "g_local.h"
-
-//=================================================================================
-
-
