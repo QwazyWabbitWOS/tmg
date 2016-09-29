6c12e9768f36fec96d09237f10b2dc669df17a2a
QwazyWabbit <gandalf.2004@gmail.com>
2016-09-29 04:49:34 -0700

    Portability of line endings and comment blocks in bot config files.

--- TMG/bot_func.c
+++ TMG/bot_func.c
@@ -16,7 +16,7 @@ qboolean Get_YenPos(char *Buff, int *curr)
 	int i = *curr + 1;
 	while(1)
 	{
-		if(Buff[i] == '\0' || Buff[i] == '\n')
+		if(Buff[i] == '\0' || Buff[i] == '\n' || Buff[i] == '\r')
 		{
 			*curr = i;
 			return true;
@@ -494,7 +494,7 @@ MESS_NOTFOUND:
 				break;
 			if(Buff[0] == '[')
 				break;
-			if(Buff[0] == '\n' || Buff[0] == '#')
+			if(Buff[0] == '\n' || Buff[0] == '\r' || Buff[0] == '#')
 			{
 				i--;
 				continue;
