fa9eb5f46498ba6a94892d68d3bd1b3c22893be7
QwazyWabbit <gandalf.2004@gmail.com>
2016-11-25 21:12:55 -0800

    Functions are extern by default.

--- TMG/filehand.h
+++ TMG/filehand.h
@@ -3,20 +3,20 @@
 #define FILEHAND_H
 
 
-extern qboolean lessGeneral(char line[], char comp[]);
-extern int checkAllowed (char *userinfo);
-extern char *NameAndIp (edict_t *ent);
-extern int IPMatch (char *clientIP, char *maskIP);
-extern qboolean entryInFile (char *filename, char ip[]);
-extern int CheckOpFile (edict_t *ent, char ip[], qboolean returnindex);
-extern qboolean CheckNameProtect (char name[], char namepass[]);
-extern qboolean ModifyOpLevel (int entry, int newlevel);
-extern int AddOperator (char entry[], int level, char pass[16]);
-extern void AddEntry (char *filename, char ip[]);
-extern void sv_ban_ip (edict_t *ent);
-extern void ShowFile(edict_t *ent, char *filename);
-extern void LogConnect (edict_t *ent, qboolean connect);
-extern void LogChat (char *text);
+qboolean lessGeneral(char line[], char comp[]);
+int checkAllowed (char *userinfo);
+char *NameAndIp (edict_t *ent);
+int IPMatch (char *clientIP, char *maskIP);
+qboolean entryInFile (char *filename, char ip[]);
+int CheckOpFile (edict_t *ent, char ip[], qboolean returnindex);
+qboolean CheckNameProtect (char name[], char namepass[]);
+qboolean ModifyOpLevel (int entry, int newlevel);
+int AddOperator (char entry[], int level, char pass[16]);
+void AddEntry (char *filename, char ip[]);
+void sv_ban_ip (edict_t *ent);
+void ShowFile(edict_t *ent, char *filename);
+void LogConnect (edict_t *ent, qboolean connect);
+void LogChat (char *text);
 
 
 #endif /* FILEHAND_H */
