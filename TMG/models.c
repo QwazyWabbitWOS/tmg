b92875c02eb6eb348079df94b9a99dfc99c7dda6
QwazyWabbit <gandalf.2004@gmail.com>
2017-10-25 23:21:29 -0700

    Touchup CheckModel.

--- TMG/models.c
+++ TMG/models.c
@@ -1,35 +1,43 @@
 
 #include "g_local.h"
 
+/**
+ Checks the models.txt file for permitted models
+ and returns 1 if permitted or if models.txt 
+ file doesn't exist.
+ Return 0 if model doesn't match any in list.
+ If we're not checking models, permit any.
+*/
 qboolean CheckModel(edict_t *ent, char *s)
 {
-	FILE    *banlist;
+	FILE    *permlist;
 	char    line[MAX_QPATH];
-	char	file_name[256];
+	char	file_name[MAX_QPATH];
 
 	if(!check_models->value)
 		return true;
 
-	Com_sprintf(file_name, sizeof file_name, "%s/%s/%s/models.txt", basedir->string, game_dir->string, cfgdir->string);
+	Com_sprintf(file_name, sizeof file_name, "%s/%s/%s/models.txt", 
+		basedir->string, game_dir->string, cfgdir->string);
 
-	banlist = fopen(file_name, "r");
-	if (banlist)
+	permlist = fopen(file_name, "r");
+	if (permlist)
 	{
-		while ( fgets(line, sizeof line, banlist) )
+		while ( fgets(line, sizeof line, permlist) )
 		{
-			line[strlen(line)-1] = '\0';
-			if ( strstr(s, line ) )
+			line[strlen(line) - 1] = '\0';
+			if ( strstr(s, line) )
 			{
-				fclose(banlist);
+				fclose(permlist);
 				return true;
 			}
 		}
-		fclose(banlist);
+		fclose(permlist);
 		return false;
 	}
 	else
 	{
-		gi.dprintf("Can't Open models.txt\n");
+		gi.dprintf("TMG WARNING: Can't Open %s\n", file_name);
 		return true;
 	}
 }
